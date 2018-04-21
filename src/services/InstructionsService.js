fbd12c090f1ec7c58a0dcb5648832903452c6b20
Laura <cbastoslaura@gmail.com>
2018-04-21 19:45:37 +0200

    Cambiar url de la API REST

--- src/services/InstructionsService.js
+++ src/services/InstructionsService.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // Servicio para recuperar las instrucciones
 export default class InstructionsService {
   get (module, level) {
-    let url = 'http://localhost/menssana/instrucciones?_format=json&nivel_instrucciones=' + level + '&modulo_instrucciones=' + module
+    let url = 'http://localhost:8079/menssana/instrucciones?_format=json&nivel_instrucciones=' + level + '&modulo_instrucciones=' + module
     return axios.get(url) // Realizamos petición GET con los parámetros level y module
   }
 }
