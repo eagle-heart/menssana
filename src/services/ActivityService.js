4ab03b81655e07d3410c68bdbf9791e0b3e24feb
Laura <cbastoslaura@gmail.com>
2018-05-21 19:46:57 +0200

    Actualizar URL de API REST

--- src/services/ActivityService.js
+++ src/services/ActivityService.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 // Servicio para recuperar las actividades
 export default class ActivityService {
   get (module, level) {
-    let url = 'http://localhost:8079/menssana/actividades?_format=json&nivel=' + level + '&modulo=' + module
+    let url = 'http://menssana-api.herokuapp.com/actividades?_format=json&nivel=' + level + '&modulo=' + module
     return axios.get(url) // Realizamos petición GET con los parámetros level y module
   }
 }
